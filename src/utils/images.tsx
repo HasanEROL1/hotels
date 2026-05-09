7810dccda323bd067c003b9f5a9fd61ae9c79352
hasanerol1 <hasanerol89@gmail.com>
2026-05-09 19:16:27 +0300

    readme

--- src/utils/images.tsx
+++
@@ -1,7 +0,0 @@
-export const typeImages = {
-    villa: { main_image: "/images/villa-1-main.jpg", gallery: ["/images/villa-1-1.jpg"] },
-    hotel: { main_image: "/images/hotel-1-main.jpg", gallery: ["/images/hotel-1-1.jpg"] },
-    apartment: { main_image: "/images/apartment-1-main.jpg", gallery: ["/images/apartment-1-1.jpg"] },
-    yayla: { main_image: "/images/yayla-1-main.jpg", gallery: ["/images/yayla-1-1.jpg"] },
-};
-export const defaultImage = "/images/default-place.jpg";
